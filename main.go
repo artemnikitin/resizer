d1556ef41f686edad6ebbcd7552853d404935113
Artem Nikitin <artemnikitin@artemnikitin.com>
2016-05-17 22:18:46 +0200

    Add support for .bmp and .webp

--- main.go
+++ main.go
@@ -11,7 +11,9 @@ import (
 	"log"
 	"os"
 
+	"github.com/chai2010/webp"
 	"github.com/nfnt/resize"
+	"golang.org/x/image/bmp"
 )
 
 var (
@@ -43,8 +45,14 @@ func main() {
 	case "png":
 		image := processPNG(file)
 		resizeIt(typ, &image)
+	case "bmp":
+		image := processBMP(file)
+		resizeIt(typ, &image)
+	case "webp":
+		image := processWEBP(file)
+		resizeIt(typ, &image)
 	default:
-		fmt.Println("Only .jpg and .png are supported at the moment!")
+		fmt.Println("Can't get an image type. Only .jpg, .png, .bmp and .webp are supported at the moment!")
 		os.Exit(1)
 	}
 }
@@ -58,6 +66,18 @@ func processJPEG(file io.Reader) image.Image {
 func processPNG(file io.Reader) image.Image {
 	image, err := png.Decode(file)
 	processError(err, "Can't convert .png file to Image")
+	return image
+}
+
+func processBMP(file io.Reader) image.Image {
+	image, err := bmp.Decode(file)
+	processError(err, "Can't convert .bmp file to Image")
+	return image
+}
+
+func processWEBP(file io.Reader) image.Image {
+	image, err := webp.Decode(file)
+	processError(err, "Can't convert .webp file to Image")
 	return image
 }
 
@@ -78,6 +98,12 @@ func getImageType(path string) string {
 		if bytes[1] == 0x50 && bytes[2] == 0x4E && bytes[3] == 0x47 {
 			result = "png"
 		}
+		if bytes[0] == 0x42 && bytes[1] == 0x4D {
+			result = "bmp"
+		}
+		if bytes[0] == 0x52 && bytes[1] == 0x49 && bytes[2] == 0x46 && bytes[3] == 0x46 {
+			result = "webp"
+		}
 	}
 	return result
 }
@@ -85,10 +111,15 @@ func getImageType(path string) string {
 func save(typ, path string, image image.Image) {
 	file, err := os.Create(path)
 	processError(err, "Can't create file to save resized image")
-	if typ == "png" {
-		png.Encode(file, image)
-	} else {
+	switch typ {
+	case "jpg":
 		jpeg.Encode(file, image, nil)
+	case "png":
+		png.Encode(file, image)
+	case "bmp":
+		bmp.Encode(file, image)
+	case "webp":
+		webp.Encode(file, image, nil)
 	}
 }
 
